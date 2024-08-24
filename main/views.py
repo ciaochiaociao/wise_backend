6735ef019953fcdabe5d9e61dae3f2207d5744af
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-08-23 19:32:50 -0700

    add more filters for datetime

--- main/views.py
+++ main/views.py
@@ -4,8 +4,16 @@ from django_filters import rest_framework as filters
 from .models import EmotionRecord
 from .serializers import EmotionRecordSerializer
 
+class EmotionRecordFilter(filters.FilterSet):
+    class Meta:
+        model = EmotionRecord
+        fields = {
+            'emotion': ['exact'],
+            'created_at': ['exact', 'range', 'year', 'time__range', 'time__gt', 'date__range', 'date__gt']
+        }
 class EmotionRecordViewSet(viewsets.ModelViewSet):
     queryset = EmotionRecord.objects.all()
     serializer_class = EmotionRecordSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ('emotion', 'created_at')
+    # filterset_fields = ('emotion', 'created_at')
+    filterset_class = EmotionRecordFilter
